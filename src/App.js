2d7d9cd49edfa66ab19cfa09296611db25a8103b
Charles Rethman <rethman@me.com>
2023-11-21 15:39:52 +1300

    Some minor testing changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Map3d from "./components/Map3d";
+// import Map3d from "./components/Map3d";
 
-export default async function App() {
+export default function App() {
   return (
     <div>
       <h2>
         This is my React component here!
       </h2>
-      await <Map3d/>
+      // await <Map3d/>
     </div>
   );
 }
